e86f588f414c2166d4f1d2d050f9088e33dd7154
Nitya Narasimhan <n.narasimhan@gmail.com>
2022-09-18 12:21:34 -0400

    Blog reset - migrating to Docusaurus

---
+++ website/blog/2022-09-18-welcome/index.md
@@ -0,0 +1,32 @@
+---
+slug: welcome
+title: Welcome Home!
+authors: [nitya, sketchthedocs, in30Days]
+tags: [nitya, sketchthedocs, in30Days]
+---
+
+# Welcome Home!!
+
+This is my umpteenth effort to start - and maintain - a personal website. And I am hopeful that this time I will make it! 
+
+Why? 
+
+Because of late I've become obsessed with technical documentation and how websites are a product's way of **rolling out a welcome mat** to people who want to learn more about it, and engage productively with its features or capabilities. 
+
+And isn't that what personal websites do? They **document a personal brand** instead of a product, and hopefully give people a sense of what that individual does, why they do it, and _ways to collaborate with them_ to build value for the community. 
+
+So here goes - an experimental sandbox site where I hope to share my three personas:
+ * **Posts authored as "Nitya"** will focus on _professional development_ building technology and soft skills that reflect my 25+ years of experience working across industry, startups, academia, and community. Plus some _personal development_ posts on parenting and community-building.
+ * **Posts authored as "SketchTheDocs"** will focus on my interest in _visual storytelling_ bringing tools like sketchnoting, visual puzzles, hand-drawn illustrations, and more, to make complex concepts easier to understand and recall for visual learners (like myself).
+ * **Posts authored as "In30Days"** is at the intersection of the two interests but with a focus on _continuous learning_. It is inspired by Tim Urban's [Life In Weeks](https://waitbutwhy.com/2014/05/life-weeks.html) about making _productive plans for using our limited time_. In my case, I want to plan month-long sprints to learn new things.
+
+## My Personal Compass
+
+At the core, personal blogs and sites are about authenticity and sharing our lived experiences in ways that help both our future selves, and those that might be following similar (or interesecting) paths in their own lives. The tech ecosystem, social media, and cultural differences often have us questioning ourselves and our values, hitting our self-esteem as we try to find meaningful goals and navigate unexpected pitfalls. Having a personal compass pointing us in the right direction, can prevent us getting lost in the noise.
+
+I have two. A north star that helps me find my way back whenever I feel myself going off-course or hitting rock bottom. And a mirror that helps me see myself the way people I love see me. They have have helped me navigate life through the ups and downs, and rediscover joy in learning, and purpose in moving forward. I hope you find your own compass for your journey. Thanks for stopping by.
+
+| My North Star| My Reflected Self |
+|:---:|:---:|
+| ![A boy stares out a window onto a campus](https://pbs.twimg.com/media/FcE7KJ-WYAAebvV?format=jpg&name=small) | ![Superhero ](https://pbs.twimg.com/media/FZKKrSJWIAYtTN9?format=jpg&name=small) |
+| My 13yo, gazing out at buildings on the Caltech campus in the summer of 2022  | A birthday gift from my young nieces - their vision of how they perceive me. |
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,132 +1,187 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+// CONFIG: Set for use in themeConfig: prism
+//    Pick: https://github.com/FormidableLabs/prism-react-renderer/tree/master/src/themes
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/vsDark");
 
 /** @type {import('@docusaurus/types').Config} */
+
+// USAGE: Config object can be accessed via React context as `{siteConfig}`
+//   See: https://docusaurus.io/docs/configuration#custom-configurations
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+  // CONFIG: Add Custom Fields - globally reference them from siteConfig
+  //    See: https://docusaurus.io/docs/deployment#using-environment-variables
+  customFields: {
+    bio: "Nitya Narasimhan is a PhD and Polyglot with 25+ years of experience building systems & software, and 10+ years of expertise in building communities and illustrating technology.",
   },
 
+  // CONFIG: Landing Pages uses this (also globally via siteConfig)
+  title: "Nitya Narasimhan, PhD",
+  tagline: "Research - Engineering - Advocacy - Illustration",
+
+  // CONIFIG; GitHub Pages
+  url: "https://nitya.dev",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "nitya", 
+  projectName: "nitya.dev", 
+  deploymentBranch: 'gh-pages',
+
+  // CONFIG: Early detection for site health
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // CONFIG: Localization if supporting multiple languages
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  // CONFIG: theme = set properties for UI like navbar, footer, docs, copyright etc.
+  //    See: https://docusaurus.io/docs/api/docusaurus-config#themeConfig
+  themeConfig:
+
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+
+      // CONFIG: sidebar
+      //    See:
+      docs :{
+        sidebar: {
+          hideable: true,
+          autoCollapseCategories: true,
+        },
+      },
+
+      // CONFIG: default theme color mode
+      //    See:
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
+
+      // CONFIG: navbar logo, items, style, stickiness
+      //    See: https://docusaurus.io/docs/next/api/themes/configuration#navbar
+      navbar: {
+        title: "nitya.dev",
+        logo: {
+          alt: "Nitya Narasimhan Profile picture",
+          src: "img/logo.jpeg",
+          href: 'https://nitya.dev/',
+          target: '_self',
+          width: 32,
+          height: 32,
+          //style: {border: 'solid white'},
+        },
+        style: 'primary',
+        items: [
+          // FIXME: TEMPORARILY DISABLE BLOG
+          //{ label: "Tutorial", position: "left", type: "doc", docId: "intro",},
+          { label: "Blog",  position: "left", to: "/blog",},
+          { label: "GitHub", position: "right", href: "https://github.com/nitya/nitya.dev"},
+          // if you use search
+          {
+            type: 'search',
+            position: 'right',
+          },
+          // custom html
+          {
+            type: 'html',
+            position: 'right',
+            value: '<button><a href="https://github.com/nitya/nitya.dev/issues/new/choose"> Report Issues </a></button>',
+          },
+        ],
+      },
+
+      // CONFIG: 
+      //    See:
+      footer: {
+        style: "light",
+        links: [
+          { label: `Twitter`, to: 'https://twitter.com/nitya' },
+          { label: `GitHub`, to: 'https://github.com/nitya' },
+          { label: `LinkedIn`, to: 'https://linkedin.com/in/nityan' },
+          { label: `Dev.to`, to: 'https://dev.to/nitya' },
+          { label: `SketchTheDocs`, to: 'https://sketchthedocs.dev' },
+          { label: `Visual Guides`, to: 'https://cloud-skills.dev' },
+          { label: `In #30Days`, to: 'https://in30Days.dev' },
+          { 
+           label: `Copyright © ${new Date().getFullYear()} Nitya Narasimhan, PhD`,
+           to: 'https://linkedin.com/in/nityan'
+          },
+        ],
+        //copyright: `Copyright © ${new Date().getFullYear()} Nitya Narasimhan, PhD`,
+      },
+
+      // CONFIG: the prism-react-renderer to highlight code blocks, add magic comments (influence code highlighting)
+      // Change: 'theme' and `darkTheme` constants at top of this config file
+      //    See: https://docusaurus.io/docs/next/api/themes/configuration#codeblock-theme
+      //    See: https://docusaurus.io/docs/next/markdown-features/code-blocks#custom-magic-comments
+      //    For additional languages e.g., 'csharp','java','js','typescript','python', 'rust', 'html','css', 'go', 'dart'
+      //    See: https://docusaurus.io/docs/next/markdown-features/code-blocks#supported-languages 
+      //    See: https://prismjs.com/#supported-languagescshar
+      prism: {
+        theme:  lightCodeTheme,      
+        darkTheme: darkCodeTheme,   
+        additionalLanguages: ['csharp','java', 'python' ],
+      },
+
+      // CONFIG: Use sparingly to drive attention to a specific call-to-action or new feature
+      //    See: https://docusaurus.io/docs/next/api/themes/configuration#announcement-bar
+      announcementBar: {
+        id: 'Follow me on LinkedIn',
+        content:
+          '<b> Follow me on <a href="https://www.linkedin.com/in/nityan/recent-activity/posts/"><b>#in/nityan</b></a>  or <a href="https://twitter.com/nitya"><b>@nitya</b></a> for latest news and activity updates</b> ',
+        backgroundColor: '#000010',
+        textColor: '#ffffff',
+        isCloseable: false,
+      },
+    }),
+
+  // CONFIG: Set presets for chosen theme
   presets: [
     [
-      'classic',
+
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+      ({  // ------- Start presets-classic options -------
+
+        // CONFIG: docs = collections of pages, tutorials, documentation
+        //    See: https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-content-docs
+        // FIXME: TEMPORARILY DISABLE BLOG
+        // docs: { sidebarPath: require.resolve("./sidebars.js"), },
+        docs: false,
+
+        // CONFIG: blog = timestamped pages, tags, site feed 
+        //    See: https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-content-blog
         blog: {
+          routeBasePath: '/', // FIXEME: Remove later - Serve the blog at the site's root
+          blogTitle: 'Learn in #30DaysOfServerless',
+          blogDescription: 'Explore Serverless Technologies, Microservices Architectures, and Developer Tools - to build your Cloud Native Applications at scale!',
+          blogSidebarCount: 'ALL',
+          blogSidebarTitle: 'Recent Articles',
+          tagsBasePath: 'tags',
+          archiveBasePath: 'archive',
+          postsPerPage: 1,
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          sortPosts: 'ascending'
         },
+
+        // CONFIG: theme = currently using `classic`
+        //    See: https://docusaurus.io/docs/api/themes/@docusaurus/theme-classic
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
-      }),
+
+      }), // ------- End preset-classic options -------
+
     ],
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
 };
 
 module.exports = config;
